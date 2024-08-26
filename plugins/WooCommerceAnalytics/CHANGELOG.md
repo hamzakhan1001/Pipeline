06fd678bf13920ddc53c3ea61d63ab17aff98263
Chris Wathen <34251245+chriswathen@users.noreply.github.com>
2024-08-26 12:26:06 -0500

    Update WooCommerceAnalytics to 5.1.2

---
+++ plugins/WooCommerceAnalytics/CHANGELOG.md
@@ -0,0 +1,92 @@
+## Changelog
+
+5.1.2 - 2024-08-26
+- Pricing updated
+
+5.1.1
+- An update to the FAQ.
+
+5.1.0
+- The plugin will now track product views, as long as the Matomo JavaScript tracker is used to track pageviews
+
+5.0.4
+- Updated README.md
+
+5.0.3
+- Add campaign parameters to plugin homepage link
+
+5.0.2
+- Update WooCommerce plugin version to 1.0.21
+
+5.0.1
+- Added code to make sure references to settings/tabs in instructions use Matomo instead of whitelabel-ed brand
+
+5.0.0
+- Compatibility with Matomo 5
+
+4.0.6
+- Update WooCommerce plugin version 1.0.20
+
+4.0.5
+ - Update WooCommerce plugin version 1.0.19
+ 
+4.0.4
+ - Update WooCommerce plugin version 1.0.18
+ 
+4.0.3
+ - Update WooCommerce plugin version 1.0.17
+ 
+4.0.2
+ - Update WooCommerce plugin version 1.0.16 for compatibility with Matomo 4
+
+4.0.1
+ - Update WooCommerce plugin version 1.0.15 fixing an issue with coupons
+
+4.0.0
+ - Compatibility with 4.0.0
+
+3.0.14
+ - Update WooCommerce plugin version 1.0.14
+ 
+3.0.13
+ - Update WooCommerce plugin version 1.0.13
+
+3.0.12
+ - Update WooCommerce plugin version 1.0.12
+
+3.0.11
+ - Update WooCommerce plugin version 1.0.11
+
+3.0.10
+ - Update WooCommerce plugin version 1.0.10
+
+3.0.9
+ - Update WooCommerce plugin version 1.0.9
+
+3.0.8
+ - Update WooCommerce plugin version 1.0.8
+ - Renamed Piwik to Matomo
+
+3.0.7
+ - Update WooCommerce plugin version 1.0.7
+
+3.0.6
+ - Update WooCommerce plugin version 1.0.6
+ 
+3.0.5
+ - Update WooCommerce plugin version 1.0.5
+ 
+3.0.4
+ - Update WooCommerce plugin version 1.0.4
+ 
+3.0.3
+ - Update WooCommerce plugin version 1.0.3
+ 
+3.0.2
+ - Update WooCommerce plugin version 1.0.2
+ 
+3.0.1
+ - Update WooCommerce plugin version 1.0.1
+
+3.0.0
+ - Initial version
---
+++ plugins/WooCommerceAnalytics/WooCommerceAnalytics.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright (C) InnoCraft Ltd - All rights reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property of InnoCraft Ltd.
+ * The intellectual and technical concepts contained herein are protected by trade secret or copyright law.
+ * Redistribution of this information or reproduction of this material is strictly forbidden
+ * unless prior written permission is obtained from InnoCraft Ltd.
+ *
+ * You shall use this code only in accordance with the license agreement obtained from InnoCraft Ltd.
+ *
+ * @link https://www.innocraft.com/
+ * @license For license details see https://www.innocraft.com/license
+ */
+
+namespace Piwik\Plugins\WooCommerceAnalytics;
+
+class WooCommerceAnalytics extends \Piwik\Plugin
+{
+    public function registerEvents()
+    {
+        return array(
+            'AssetManager.getStylesheetFiles' => 'getStylesheetFiles',
+            'Translate.getClientSideTranslationKeys' => 'getClientSideTranslationKeys',
+        );
+    }
+
+    public function getStylesheetFiles(&$stylesheets)
+    {
+        $stylesheets[] = "plugins/WooCommerceAnalytics/stylesheets/styles.less";
+    }
+
+    public function getClientSideTranslationKeys(&$result)
+    {
+        $result[] = 'Installation_Installation';
+        $result[] = 'WooCommerceAnalytics_WoocommerceIntegration';
+        $result[] = 'WooCommerceAnalytics_InstallDownload2';
+        $result[] = 'WooCommerceAnalytics_InstallLoginWordPress';
+        $result[] = 'WooCommerceAnalytics_InstallWordPress';
+        $result[] = 'WooCommerceAnalytics_InstallActivatePlugin';
+        $result[] = 'WooCommerceAnalytics_InstallGoSettings2';
+        $result[] = 'WooCommerceAnalytics_InstallConfigure';
+        $result[] = 'WooCommerceAnalytics_DownloadPlugin';
+        $result[] = 'WooCommerceAnalytics_UpdatingPlugin';
+        $result[] = 'WooCommerceAnalytics_UpdatingPluginConfigured2';
+        $result[] = 'WooCommerceAnalytics_UpdatingPluginPiwikServer';
+    }
+}
