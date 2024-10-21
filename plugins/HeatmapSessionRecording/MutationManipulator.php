65f9ec10bce95de3173ced925f766abef908a14f
Chris Wathen <34251245+chriswathen@users.noreply.github.com>
2024-10-21 10:27:24 -0500

    Update HeatmapSessionRecording to 5.1.8

--- plugins/HeatmapSessionRecording/MutationManipulator.php
+++ plugins/HeatmapSessionRecording/MutationManipulator.php
@@ -210,8 +210,12 @@ class MutationManipulator
         if (
             !empty($value) &&
             (
-                stripos($value, 'script') !== FALSE || // Covers all 3 cases script, javascript and ecmascript
-                strpos($value, '&#') !== FALSE // Wll handle decimal cases without trailing semicolon eg: <IMG SRC=&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058&#0000097&#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041>
+                stripos($value, 'script:') !== false ||
+                stripos($value, 'javascript') !== false ||
+                stripos($value, 'ecmascript') !== false ||
+                stripos($value, '<script') !== false ||
+                stripos($value, 'script>') !== false ||
+                strpos($value, '&#') !== false // Wll handle decimal cases without trailing semicolon eg: <IMG SRC=&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058&#0000097&#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041>
             )
         ) {
             return true;
