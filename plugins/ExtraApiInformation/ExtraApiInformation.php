110e46b57753f36dcdf1f1ef0d064305e8151f42
Chris Wathen <34251245+chriswathen@users.noreply.github.com>
2025-01-02 15:19:31 -0600

    Add ExtraApiInformation

---
+++ plugins/ExtraApiInformation/ExtraApiInformation.php
@@ -0,0 +1,67 @@
+<?php 
+/**
+ * Plugin Name: Extra Api Information (Matomo Plugin)
+ * Plugin URI: http://plugins.matomo.org/ExtraApiInformation
+ * Description: Exposes some information via API that is not normally available
+ * Author: Digitalist Open Cloud, Mikke Schirén
+ * Author URI: https://github.com/Digitalist-Open-Cloud/Matomo-Plugin-ExtraApiInformation
+ * Version: 0.1.3
+ */
+?><?php
+
+/**
+ * The Extra Api Information plugin for Matomo.
+ *
+ * Copyright (C) 2024 Digitalist Open Cloud <cloud@digitalist.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Piwik\Plugins\ExtraApiInformation;
+
+ 
+if (defined( 'ABSPATH')
+&& function_exists('add_action')) {
+    $path = '/matomo/app/core/Plugin.php';
+    if (defined('WP_PLUGIN_DIR') && WP_PLUGIN_DIR && file_exists(WP_PLUGIN_DIR . $path)) {
+        require_once WP_PLUGIN_DIR . $path;
+    } elseif (defined('WPMU_PLUGIN_DIR') && WPMU_PLUGIN_DIR && file_exists(WPMU_PLUGIN_DIR . $path)) {
+        require_once WPMU_PLUGIN_DIR . $path;
+    } else {
+        return;
+    }
+    add_action('plugins_loaded', function () {
+        if (function_exists('matomo_add_plugin')) {
+            matomo_add_plugin(__DIR__, __FILE__, true);
+        }
+    });
+}
+
+class ExtraApiInformation extends \Piwik\Plugin
+{
+    public function registerEvents()
+    {
+        return [
+            'CronArchive.getArchivingAPIMethodForPlugin' => 'getArchivingAPIMethodForPlugin',
+        ];
+    }
+
+    // support archiving just this plugin via core:archive
+    public function getArchivingAPIMethodForPlugin(&$method, $plugin)
+    {
+        if ($plugin == 'ExtraApiInformation') {
+            $method = 'ExtraApiInformation.getExampleArchivedMetric';
+        }
+    }
+}
