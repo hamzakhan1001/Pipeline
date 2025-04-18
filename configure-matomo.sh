0a5e84acb18e420ccf8f35a7e27e7493e199f5bb
Hamza <hamza.khan@focusteck.com>
2025-04-18 15:38:00 +0500

    Permissions

--- configure-matomo.sh
+++ configure-matomo.sh
@@ -9,23 +9,8 @@ fi
 
 plugins=("AbTesting" "ActivityLog"  "Cohorts" "CrashAnalytics" "CustomReports" "Funnels" "LoginSaml" "MediaAnalytics" "MultiChannelConversionAttribution" "RollUpReporting" "SEOWebVitals" "SearchEngineKeywordsPerformance" "UsersFlow" "WhiteLabel")
 db_file="/var/www/html/plugins/GhostBrand/files/initial_db.sql"
-db_host="matomo.cyabb6bvejrw.us-east-1.rds.amazonaws.com"
-db_user="admin"
-db_pass="admin1234"
-db_name="$MATOMO_DATABASE_DBNAME"
-
-# Check if database has any tables
-table_count=$(mysql -h "$db_host" -u "$db_user" -p"$db_pass" -D "$db_name" -sse "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='$db_name';")
-
-if [ "$table_count" -gt 0 ]; then
-    echo "Database '$db_name' already contains tables ($table_count). Skipping import."
-    exit 0
-fi
-
-# If no tables, proceed with import
 if [ -f "$db_file" ]; then
-    echo "No tables found in '$db_name'. Importing database from $db_file..."
-    mysql -h "$db_host" -u "$db_user" -p"$db_pass" "$db_name" < "$db_file"
+    mysql -h matomo.cyabb6bvejrw.us-east-1.rds.amazonaws.com -u admin -p"admin1234" "$MATOMO_DATABASE_DBNAME" < "$db_file"
     if [ $? -eq 0 ]; then
         echo "Database imported successfully."
     else
@@ -111,7 +96,7 @@ fi
 touch /var/log/permission.log
 touch /etc/cron.d/fix-permissions
 chmod 0644 /etc/cron.d/fix-permissions
-echo "* * * * * root chown -R www-data:www-data /var/www/html/tmp && chmod -R 775 /var/www/html/tmp && chmod a+w /var/www/html/tmp/climulti /var/www/html/tmp/latest /var/www/html/tmp/cache /var/www/html/tmp/logs /var/www/html/tmp/sessions /var/www/html/tmp/tcpdf /var/www/html/tmp/templates_c && echo \"\$(date) - Ownership and permissions updated successfully.\" >> /var/log/permission.log" > /etc/cron.d/fix-permissions
+echo "    * root chown -R www-data:www-data /var/www/html/tmp && chmod -R 775 /var/www/html/tmp && chmod a+w /var/www/html/tmp/climulti /var/www/html/tmp/latest /var/www/html/tmp/cache /var/www/html/tmp/logs /var/www/html/tmp/sessions /var/www/html/tmp/tcpdf /var/www/html/tmp/templates_c && echo \"\$(date) - Ownership and permissions updated successfully.\" >> /var/log/permission.log" > /etc/cron.d/fix-permissions
 
 cd /var/www
 mkdir -p custom-code
@@ -119,15 +104,4 @@ cd /var/www/html
 mv Dockerfile configure-matomo.sh default index.nginx-debian.html nginx.conf ../custom-code
 echo "Custom code moved to another folder to maintain Matomo integrity."
 
-echo "Ghost Cloud configuration completed Successfully."
-#./console core:update
-# Activate the TagManager plugin so it can create a container
-# ./console plugin:activate TagManager TagManagerExtended
-# # Run updates for the activated plugins
-
-# if [ $? -eq 0 ]; then
-#     echo "Plugins updated successfully."
-# else
-#     echo "Failed to update plugins."
-#     exit 1
-# fi
+echo "Matomo configuration completed."
