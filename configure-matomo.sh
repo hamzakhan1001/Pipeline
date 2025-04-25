0a14f588e1adc179fb39ceb502b2d6a7f05881de
adil-ali-dev <adil.ali.dev@gmail.com>
2025-04-25 17:07:54 +0500

    DB handling

--- configure-matomo.sh
+++ configure-matomo.sh
@@ -7,7 +7,7 @@ if [ -z "$MATOMO_DATABASE_DBNAME" ]; then
     exit 1
 fi
 
-plugins=("AbTesting" "ActivityLog" "Cohorts" "CrashAnalytics" "CustomReports" "Funnels" "LoginSaml" "MediaAnalytics" "MultiChannelConversionAttribution" "RollUpReporting" "SEOWebVitals" "SearchEngineKeywordsPerformance" "UsersFlow" "WhiteLabel")
+plugins=("AbTesting" "ActivityLog"  "Cohorts" "CrashAnalytics" "CustomReports" "Funnels" "LoginSaml" "MediaAnalytics" "MultiChannelConversionAttribution" "RollUpReporting" "SEOWebVitals" "SearchEngineKeywordsPerformance" "UsersFlow" "WhiteLabel")
 db_file="/var/www/html/plugins/GhostBrand/files/initial_db.sql"
 db_host="matomo.cyabb6bvejrw.us-east-1.rds.amazonaws.com"
 db_user="admin"
@@ -18,7 +18,7 @@ db_name="$MATOMO_DATABASE_DBNAME"
 table_count=$(mysql -h "$db_host" -u "$db_user" -p"$db_pass" -D "$db_name" -sse "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='$db_name';")
 
 if [ "$table_count" -gt 0 ]; then
-    echo "Database '$db_name' already contains $table_count tables. Skipping import and config update."
+    echo "Database '$db_name' already contains tables ($table_count). Skipping import."
 else
     # If no tables, proceed with import
     if [ -f "$db_file" ]; then
@@ -26,18 +26,6 @@ else
         mysql -h "$db_host" -u "$db_user" -p"$db_pass" "$db_name" < "$db_file"
         if [ $? -eq 0 ]; then
             echo "Database imported successfully."
-
-            # Update database name in config.ini.php **only now after successful import**
-            config_file="/var/www/html/config/config.ini.php"
-
-            if grep -q "^dbname" "$config_file"; then
-                sed -i "s/^dbname.*/dbname = \"$MATOMO_DATABASE_DBNAME\"/" "$config_file"
-            else
-                sed -i "/^\[database\]/a dbname = \"$MATOMO_DATABASE_DBNAME\"" "$config_file"
-            fi
-
-            echo "Database name updated successfully in config.ini.php."
-
         else
             echo "Failed to import database."
             exit 1
@@ -47,6 +35,17 @@ else
         exit 1
     fi
 fi
+
+# Update database name in config.ini.php
+config_file="/var/www/html/config/config.ini.php"
+
+if grep -q "^dbname" "$config_file"; then
+    sed -i "s/^dbname.*/dbname = \"$MATOMO_DATABASE_DBNAME\"/" "$config_file"
+else
+    sed -i "/^\[database\]/a dbname = \"$MATOMO_DATABASE_DBNAME\"" "$config_file"
+fi
+
+echo "Database name updated successfully in config.ini.php."
 
 cd /var/www/html/
 chmod +x /var/www/html/console
@@ -64,19 +63,20 @@ chown -R www-data:www-data /var/www/html/tmp/cache/tracker/
 
 ./console user:reset-password --login=ghost.superuser --new-password=admin1234
 
-echo "Activating all plugins..."
+# echo "Activating all plugins..."
 
-for plugin in "${plugins[@]}"; do
-    echo "Activating plugin: $plugin"
-    ./console plugin:activate "$plugin"
-    if [ $? -eq 0 ]; then
-        echo "✅ Plugin $plugin activated successfully."
-    else
-        echo "❌ Failed to activate plugin $plugin."
-    fi
-done
+# for plugin in "${plugins[@]}"; do
+#     echo "Activating plugin: $plugin"
+#     ./console plugin:activate "$plugin"
+    
+#     if [ $? -eq 0 ]; then
+#         echo "✅ Plugin $plugin activated successfully."
+#     else
+#         echo "❌ Failed to activate plugin $plugin."
+#     fi
+# done
 
-echo "All plugins processed."
+# echo "All plugins processed."
 
 # Setup postfix service
 echo "Configuring Postfix..."
@@ -85,10 +85,11 @@ postconf -e 'inet_protocols = ipv4'
 postconf -e 'mydestination = localhost'
 postconf -e 'relayhost ='
 
+# Restart Postfix service
 service postfix restart
 echo "Postfix configured successfully."
 
-# Setup cron job for Matomo archiving
+# Setup a cron job for Matomo archiving
 echo "*/5 * * * * /usr/bin/php /var/www/html/console core:archive --matomo-domain=$MATOMO_CLIENT_DOMAIN > /var/log/cronjob.log 2>&1" | crontab -
 
 if [ $? -eq 0 ]; then
@@ -98,14 +99,14 @@ else
     exit 1
 fi
 
-# Setup cron job for Permissions
-echo "Updating Ghost Cloud Permissions"
-touch /var/log/permission.log
-mkdir -p /var/www/html/tmp/cache/tracker/
-cat << 'EOF' > /etc/cron.d/fix-permissions
-* * * * * root chown -R www-data:www-data /var/www/html/tmp/ /var/www/html/tmp/cache/tracker/ /var/www/html/tmp/templates_c/29 && chmod -R 0755 /var/www/html/tmp/ /var/www/html/tmp/cache/tracker/ /var/www/html/tmp/templates_c/29 && chmod a+w /var/www/html/tmp/climulti /var/www/html/tmp/latest /var/www/html/tmp/cache /var/www/html/tmp/logs /var/www/html/tmp/sessions /var/www/html/tmp/tcpdf /var/www/html/tmp/templates_c && echo "$(date) - Ownership and permissions updated successfully." >> /var/log/permission.log
-EOF
-chmod 0644 /etc/cron.d/fix-permissions
+# # Setup cron job for Permissions
+# echo "Updating Ghost Cloud Permissions"
+# touch /var/log/permission.log
+# mkdir -p /var/www/html/tmp/cache/tracker/
+# cat << 'EOF' > /etc/cron.d/fix-permissions
+# * * * * * root chown -R www-data:www-data /var/www/html/tmp/ /var/www/html/tmp/cache/tracker/ /var/www/html/tmp/templates_c/29 && chmod -R 0755 /var/www/html/tmp/ /var/www/html/tmp/cache/tracker/ /var/www/html/tmp/templates_c/29 && chmod a+w /var/www/html/tmp/climulti /var/www/html/tmp/latest /var/www/html/tmp/cache /var/www/html/tmp/logs /var/www/html/tmp/sessions /var/www/html/tmp/tcpdf /var/www/html/tmp/templates_c && echo "$(date) - Ownership and permissions updated successfully." >> /var/log/permission.log
+# EOF
+# chmod 0644 /etc/cron.d/fix-permissions
 
 cd /var/www
 mkdir -p custom-code
@@ -113,4 +114,4 @@ cd /var/www/html
 mv Dockerfile configure-matomo.sh default index.nginx-debian.html nginx.conf ../custom-code
 echo "Custom code moved to another folder to maintain Matomo integrity."
 
-echo "Matomo configuration completed."
+echo "Ghost Cloud configuration completed Successfully."
