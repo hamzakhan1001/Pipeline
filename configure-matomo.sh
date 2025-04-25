e8f335494815f9589f20b2b5cafebbf1e2cf31bc
adil-ali-dev <adil.ali.dev@gmail.com>
2025-04-25 14:44:55 +0500

    Testing

--- configure-matomo.sh
+++ configure-matomo.sh
@@ -98,12 +98,6 @@ else
     exit 1
 fi
 
-cd /var/www
-mkdir -p custom-code
-cd /var/www/html
-mv Dockerfile configure-matomo.sh default index.nginx-debian.html nginx.conf ../custom-code
-echo "Custom code moved to another folder to maintain Matomo integrity."
-
 echo "Adding Cronjob For Updating Ghost Cloud Permissions"
 * * * * * root bash -c 'chown -R www-data:www-data /var/www/html/tmp/ && \
 chown -R www-data:www-data /var/www/html/tmp/templates_c/29 && \
@@ -111,6 +105,12 @@ chmod -R 0755 /var/www/html/tmp/ && \
 chmod -R 0755 /var/www/html/tmp/templates_c/29 && \
 rm -rf /var/www/html/tmp/cache/* && \
 echo "$(date) - Ownership and permissions updated successfully." >> /var/log/permission.log'
+
+cd /var/www
+mkdir -p custom-code
+cd /var/www/html
+mv Dockerfile configure-matomo.sh default index.nginx-debian.html nginx.conf ../custom-code
+echo "Custom code moved to another folder to maintain Matomo integrity."
 
 # echo "Updating Ghost Cloud Permissions"
 # chown -R www-data:www-data /var/www/html/tmp/
