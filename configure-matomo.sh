82432952ae2b87c51d13f6769d2ccfd3c33c957d
adil-ali-dev <adil.ali.dev@gmail.com>
2025-05-12 13:24:48 +0500

    Permissions

--- configure-matomo.sh
+++ configure-matomo.sh
@@ -121,12 +121,13 @@ echo "Custom code moved to another folder to maintain Matomo integrity."
 # Correct permissions
 chown -R www-data:www-data /var/www/html/tmp
 chmod -R 775 /var/www/html/tmp
-# chmod a+w /var/www/html/tmp/climulti
-# chmod a+w /var/www/html/tmp/lates
+chmod a+w /var/www/html/tmp/climulti
+chmod a+w /var/www/html/tmp/lates
 mkdir -p /var/www/html/tmp/templates_c/29
 chmod -R a+w /var/www/html/tmp/templates_c
 chown -R www-data:www-data /var/www/html/tmp/templates_c
-
+find /var/www/html/tmp -type d -exec chmod 775 {} \;
+find /var/www/html/tmp -type f -exec chmod 664 {} \;
 find /var/www/html/tmp -type d -exec chmod g+s {} \;
 echo "Permissions fixed for tmp directory."
 
